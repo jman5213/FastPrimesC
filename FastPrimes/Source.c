fe47737b9887bd836e4be80664549c5b79c15239
jman5213 <jman5213@outlook.com>
2024-08-21 08:33:19 -0500

    might work, need to test

--- FastPrimes/Source.c
+++ FastPrimes/Source.c
@@ -7,18 +7,45 @@ bool prime(int num)
 	int sqr = (int)sqrt(num) + 1;
 	for (int div = 5; div >= sqr; div += 5)
 	{
-		return 0;
+		if(num%div==0 || num%(div+2)==0)
+		{
+			return false
+		}
 	}
-	return num;
+	return true;
+}
+
+int mainLoop()
+{
+	int calcTo;
+	printf("Calculate the nth prime: ");
+	calcTo = scanf("%d", &calcTo); //needs input
+	int primesFound = 2;
+	int currentNum = 1;
+
+	while (primesFound < calcTo) {
+		currentNum += 4;
+		primesFound += prime(currentNum);
+
+		if (primesFound >= calcTo)
+		{
+			return currentNum
+		}
+
+		currentNum += 2;
+		primesFound += prime(currentNum);
+	}
+
+	return currentNum
 }
 
 int main() {
 	
-	int calcTo;
 	const float time1 = 0.0; //needs time
-	printf("Calculate the nth prime: ");
-	calcTo = scanf("%d", &calcTo); //needs input
+	int ans = mainLoop();
+	const float time2 = 0.0;
 
+	printf("Your #: %i\n Time: %fsec",num, (time2-time1))
 
 	system("pause");
 	return 0;
